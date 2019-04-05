d14da44c821ac2435a0d16a52f04a468f9d46557
Elvis Alcántara Pinedo <aelvis@users.noreply.github.com>
2019-04-05 15:51:01 -0500

    actualizar
    actualizar

--- src/app/component/boleta/devolucion-total/devolucion-total.component.ts
+++ src/app/component/boleta/devolucion-total/devolucion-total.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../../services/usuario.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-devolucion-total',
@@ -7,11 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevolucionTotalComponent implements OnInit {
 	public devolucion_total:boolean;
-  	constructor() { 
+  	constructor(private toastr: ToastrService,private _usu: UsuarioService, private _router: Router, private route: ActivatedRoute) { 
   		this.devolucion_total = true;
   	}
-
-  	ngOnInit() {
-  	}
+	ngOnInit() {
+	}
+	showSuccess(titulo,mensaje) {
+	    this.toastr.success(mensaje, titulo);
+	}
+	showError(titulo,mensaje) {
+	   this.toastr.error(mensaje, titulo);
+	}
+	showWarning(titulo,mensaje) {
+	    this.toastr.warning(mensaje, titulo);
+	}
 
 }
